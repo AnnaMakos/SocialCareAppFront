b65bdd9fd437b810f1979c9f077094cd3397b5dd
AnnaMakos <pointa96@gmail.com>
2020-04-02 18:21:54 +0200

    form adding works

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -3,6 +3,7 @@ import { UserDTO } from '../model/user.model';
 import { UserService } from '../service/user.service';
 import { ApplicationFormService } from '../service/application.form.service';
 import { MatSnackBar, MatTableDataSource } from '@angular/material';
+import { ApplicationFormDTO } from '../model/application-form.model';
 
 @Component({
   selector: 'app-application-form',
@@ -13,14 +14,7 @@ export class ApplicationFormComponent implements OnInit {
 
   currentUser: UserDTO;
   institutionName: string;
-  numberList = [1, 2, 3];
   childrenList = [];
-
-
-  constructor(private userService: UserService,
-    private applicationFormService: ApplicationFormService,
-    private snackBar: MatSnackBar) { }
-
   applicationStatus: string = 'SEND';
   maritalStatus: string;
   citizenship: string;
@@ -28,22 +22,44 @@ export class ApplicationFormComponent implements OnInit {
   comments: string = '';
   username: string = "anja";
   numberOfChildren: number = 0;
+  numberOfChildrenChosen: Promise<boolean>;
+  currentUserLoaded: Promise<boolean>;
+  currentFormId: number;
+  currentForm: ApplicationFormDTO;
+
+  childrenName = [];
+  childrenSurname = [];
+  childrenPesel = [];
+  childrenCitizenship = [];
+
+  constructor(private userService: UserService,
+    private applicationFormService: ApplicationFormService,
+    private snackBar: MatSnackBar) { }
+
+
 
   ngOnInit() {
     this.refresh();
   }
 
   refresh() {
-    this.userService.getCurrentUser().subscribe(user =>
-      this.currentUser = user)
+    this.userService.getCurrentUser().subscribe(user => {
+      this.currentUser = user;
+      this.currentUserLoaded = Promise.resolve(true);
+    }
+    )
 
   }
 
   addForm() {
-    console.log("phone " + this.phone);
-    console.log("comments " + this.comments);
-    console.log("maritals " + this.maritalStatus);
-    console.log("number " + this.numberOfChildren);
+    //   console.log("phone " + this.phone);
+    //   console.log("comments " + this.comments);
+    //   console.log("maritals " + this.maritalStatus);
+    //   console.log("number " + this.numberOfChildren);
+    //   for (var i = 0; i < this.numberOfChildren; i++) {
+    //     console.log("imie dziecka " + this.childrenName[i] + " nr " + i);
+    //     console.log("obywatelstwo dziecka " + this.childrenCitizenship[i] + " nr " + i);
+    //   }
 
     this.applicationFormService.addApplicationForm(
       this.applicationStatus,
@@ -53,20 +69,35 @@ export class ApplicationFormComponent implements OnInit {
       this.comments,
       this.username
     ).subscribe(data => {
+      // this.currentFormId = data.id;
+      this.currentForm = data;
+      this.currentFormId = this.currentForm.id;
+      console.log("to jest data >>>> " + data.id);
+      console.log("citiz data >>>> " + data.citizenship);
+      console.log("to jest id formularza >>>>> " + this.currentFormId);
+      console.log("current form >>>> "+ this.currentForm);
+      console.log("current form id bezposredno >>>> "+ this.currentForm.id);
+      for (var i = 0; i < this.numberOfChildren; i++) {
+        this.applicationFormService.addChildForm(
+          this.childrenName[i],
+          this.childrenSurname[i],
+          this.childrenPesel[i],
+          this.childrenCitizenship[i],
+          this.currentFormId
+        ).subscribe()
+      }
       this.openSnackBar("Wniosek został wysłany", "OK");
     }
     )
   }
 
   setChildrenNumber(childNum: number) {
-    console.log("number " + this.numberOfChildren);
     this.numberOfChildren = childNum;
-    console.log("number " + this.numberOfChildren);
 
-    for(var i = 0; i < this.numberOfChildren; i++) {
+    for (var i = 0; i < this.numberOfChildren; i++) {
       this.childrenList[i] = i;
-      console.log(" list: " + this.childrenList[i]);
     }
+    this.numberOfChildrenChosen = Promise.resolve(true);
   }
 
   openSnackBar(message: string, action: string) {
--- src/app/service/application.form.service.ts
+++ src/app/service/application.form.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ChildFormDTO } from '../model/child-form.model';
+import { ApplicationFormDTO } from '../model/application-form.model';
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
 @Injectable()
 export class ApplicationFormService {
@@ -15,7 +20,7 @@ export class ApplicationFormService {
         phone: string,
         comments: string,
         username: string
-    ): Observable<any> {
+    ): Observable<ApplicationFormDTO> {
 
         const form = {
             applicationStatus: applicationStatus,
@@ -25,11 +30,26 @@ export class ApplicationFormService {
             comments: comments,
             username: username
         }
+        
+        return this.http.post<ApplicationFormDTO>(this.applicationUrl + 'addform', form);
+    }
 
-        const options = {
-            responseType: 'text' as 'json'
+    public addChildForm(
+        name: string,
+        surname: string,
+        pesel: string,
+        citizenship,
+        id: number
+    ): Observable<ChildFormDTO> {
+
+        const form = {
+            name: name,
+            surname: surname,
+            pesel: pesel,
+            citizenship: citizenship
         }
-        return this.http.post<any>(this.applicationUrl + 'addform', form, options);
+
+        return this.http.post<ChildFormDTO>(this.applicationUrl + "addchild/" + id, form);
     }
 
 }
